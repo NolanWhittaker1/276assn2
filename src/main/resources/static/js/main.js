4894673dec42b97c0c159711adb224bb82b25a6c
Nolan Whittaker <nolan_whittaker@sfu.ca>
2024-02-25 18:55:26 -0800

    Final changes

--- src/main/resources/static/js/main.js
+++
@@ -1,9 +0,0 @@
-function handleQuestionButtonClick(studentId) {
-    console.log("Question button clicked for student ID: " + studentId);
-    // Add your logic here
-}
-
-function handleQuestionFowardClick(studentId) {
-    console.log("X button clicked for student ID: " + studentId);
-    // Add your logic here
-}
